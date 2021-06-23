217b694d54017849b5da22cb513cd8738c8f69d4
troke12 <jame_glove@yahoo.com>
2021-06-23 17:26:15 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Simple Backup Scripts
+
+We do not use mysqldump because its dumb
+
+# Requirements
+
+- Mounted Google Drive on the Server
+- Handsome
+
+# Usage
+
+`./backup.sh`
+
+# Configuration
+
+Make your config before start the scripts
+
+`crot.config`
+```
+webhookDC="INSERT_YOUR_DISCORD_WEBHOOK"
+```
+
+Then edit `backup.sh` locate your config file
---
+++ backup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#config path
+source ~/db_backup/crot.config
+
+#variable
+dateNow=$(date +%Y-%m-%d-%H)
+filename="datenshi_backup_${dateNow}.zip" #edit this
+
+# Prosesssssssss
+echo "File backup belum ada dan mencoba untuk membuat backup"
+curl -X POST --data '{"content": "`[SERVER]` Memulai proses backup..."}' --header "Content-Type:application/json" $webhookDC
+sleep 3
+# Edit as you needed
+zip -u -r "$HOME/db_backup/$filename" /var/lib/mysql/{datenshi,minecraft,ibdata1,ib_logfile0,ib_logfile1}
+echo "Proses Zip sudah selesai"
+sleep 3
+#  Your file being copied to your Google Drive
+cp "$HOME/db_backup/$filename" ~/GDRIVE-BACKUP/backup/
+echo "Upload ke GoogleDrive Complete!"
+curl -X POST --data '{"content": "`[SERVER]` Backup **osu!** dan **minecraft** sudah selesai!"}' --header "Content-Type:application/json" $webhookDC
+exit 0;
